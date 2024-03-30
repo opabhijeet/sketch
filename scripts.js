e68128eec4db0ca638640b97b0a4314e1d82f99c
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-03-31 05:09:54 +0530

    create custom size grid

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-let side = 16;
-
-function createGrid(){
-
+function createGrid(side=16){
     const container = document.querySelector("#container");
     for(let i=1;i<=side;i++){
         const row = document.createElement("div");
@@ -14,5 +11,19 @@ function createGrid(){
         container.appendChild(row);
     }
 }
-
 createGrid();
+
+function getInput(){
+    let sides = +prompt("Invalid choice!!!\n Enter number of boxes you want on each side.\n(must not be more than 100)");
+    if(isNaN(sides) || sides>100) return getInput();
+    return sides;
+}
+
+const reset = document.querySelector("#create");
+reset.addEventListener("click",()=>{
+    const container = document.querySelector("#container");
+    container.textContent = "";
+    let sides = +prompt("Enter number of boxes you want on each side.\n(must not be more than 100)");
+    if(isNaN(sides) || sides>100) sides = getInput();
+    createGrid(sides);
+});
