be11cbf3dede4972a1f9c637fc10a50c86d16de1
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-03-31 13:09:48 +0530

    randomise color

--- scripts.js
+++ scripts.js
@@ -28,10 +28,18 @@ reset.addEventListener("click",()=>{
     createGrid(sides);
     if(sides === 0) container.style.border = "1px solid black";
 });
-
+function getColor(){
+    const hexValues = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    let color="#";
+    for(let i=1;i<=6;i++){
+        color = color + hexValues[Math.floor(Math.random()*16)];
+    }
+    console.log(color);
+    return color;
+}
 function color(e){
     if(e.target === container) return;
-    e.target.style.backgroundColor = "black";
+    e.target.style.backgroundColor = getColor();
 }
 container.addEventListener("mousedown", (e)=>{
     if(e.button === 0) container.addEventListener("mouseover", color);
