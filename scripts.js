e2a5c9f30249bf169cd5608d15a947f9f73ad8e7
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-03-31 13:23:15 +0530

    solve drag issue
    sove the issue of mouseup not triggering on drag

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 const container = document.querySelector("#container");
 
-function createGrid(side=16){
+function createGrid(side=16){   //not using warp so that every row has equal boxes
     for(let i=1;i<=side;i++){
         const row = document.createElement("div");
         row.classList.add("row");
         for(let i=1;i<=side;i++){
             const box = document.createElement("div");
             box.setAttribute("id","box");
+            box.ondragstart = ()=> false;       //solved the drag issue
             row.appendChild(box);
         }
         container.appendChild(row);
@@ -28,7 +29,7 @@ reset.addEventListener("click",()=>{
     createGrid(sides);
     if(sides === 0) container.style.border = "1px solid black";
 });
-function getColor(){
+function getColor(){    //random color generator
     const hexValues = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
     let color="#";
     for(let i=1;i<=6;i++){
@@ -41,10 +42,17 @@ function color(e){
     if(e.target === container) return;
     e.target.style.backgroundColor = getColor();
 }
-container.addEventListener("mousedown", (e)=>{
-    if(e.button === 0) container.addEventListener("mouseover", color);
+let active = false;
+container.addEventListener("mousedown", (e)=>{ 
+    if(e.button === 0){
+        container.addEventListener("mouseover", color);  //start coloring on mouse-down
+        active = true;
+    }
 });
-container.addEventListener("mouseup", (e)=>{
-    if(e.button === 0) container.removeEventListener("mouseover", color);
+container.addEventListener("mouseup", (e)=>{    
+    if(active){
+        container.removeEventListener("mouseover", color);  //end coloring on mouse-up
+        active=false;
+    }
 });
-container.addEventListener("click", e => e.target.style.backgroundColor = "white");
+container.addEventListener("click", e => e.target.style.backgroundColor = "white"); //reset the box
