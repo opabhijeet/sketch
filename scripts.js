51d3738e472a9197528fffeec30b6034fb75bb5b
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-03-31 12:56:38 +0530

    make grid on click

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
+const container = document.querySelector("#container");
+
 function createGrid(side=16){
-    const container = document.querySelector("#container");
     for(let i=1;i<=side;i++){
         const row = document.createElement("div");
         row.classList.add("row");
@@ -21,9 +22,21 @@ function getInput(){
 
 const reset = document.querySelector("#create");
 reset.addEventListener("click",()=>{
-    const container = document.querySelector("#container");
     container.textContent = "";
     let sides = +prompt("Enter number of boxes you want on each side.\n(must not be more than 100)");
     if(isNaN(sides) || sides>100) sides = getInput();
     createGrid(sides);
+    if(sides === 0) container.style.border = "1px solid black";
 });
+
+function color(e){
+    if(e.target === container) return;
+    e.target.style.backgroundColor = "black";
+}
+container.addEventListener("mousedown", (e)=>{
+    if(e.button === 0) container.addEventListener("mouseover", color);
+});
+container.addEventListener("mouseup", (e)=>{
+    if(e.button === 0) container.removeEventListener("mouseover", color);
+});
+container.addEventListener("click", e => e.target.style.backgroundColor = "white");
